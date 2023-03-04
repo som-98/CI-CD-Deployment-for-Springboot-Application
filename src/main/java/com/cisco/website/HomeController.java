d059d0a28376b18e69e9b01ad5ded72d696c7997
som-98 <somnathnandi98@gmail.com>
2023-03-05 01:06:07 +0530

    first commit

---
+++ src/main/java/com/cisco/website/HomeController.java
@@ -0,0 +1,19 @@
+package com.cisco.website;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello, Spring Boot!");
+        return "home.html";
+    }
+    
+}
+
+
